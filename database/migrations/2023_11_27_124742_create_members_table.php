d440f61663e0f857b8110549c4e64d3688f5f8d0
Paul Christian Rueda <nexuslifeline@gmail.com>
2023-11-27 21:09:30 +0800

    Simplify field to just use name instead

--- database/migrations/2023_11_27_124742_create_members_table.php
+++ database/migrations/2023_11_27_124742_create_members_table.php
@@ -14,13 +14,10 @@ return new class extends Migration
         Schema::create('members', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->index();
-            $table->string('first_name')->nullable();
-            $table->string('middle_name')->nullable();
-            $table->string('last_name')->nullable();
+            $table->string('name')->nullable();
             $table->string('phone_no')->nullable();
             $table->string('mobile_no')->nullable();
             $table->string('address')->nullable();
-            $table->date('birth_date');
 
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
--- database/migrations/2023_11_27_130416_create_admins_table.php
+++ database/migrations/2023_11_27_130416_create_admins_table.php
@@ -14,9 +14,7 @@ return new class extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->index();
-            $table->string('first_name')->nullable();
-            $table->string('middle_name')->nullable();
-            $table->string('last_name')->nullable();
+            $table->string('name')->nullable();
             $table->string('phone_no')->nullable();
             $table->string('mobile_no')->nullable();
             $table->string('address')->nullable();
