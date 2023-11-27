c91fc06c28c33de9cbf87da0d6b15607bfe8c175
Paul Christian Rueda <nexuslifeline@gmail.com>
2023-11-27 08:55:18 +0800

    Add org migrations

--- database/migrations/2023_11_27_004024_create_organizations_table.php
+++ database/migrations/2023_11_27_004024_create_organizations_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->index();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->nullable();
+            $table->string('phone_no', 30)->nullable();
+            $table->string('mobile_no', 30)->nullable();
+            $table->string('address')->nullable();
+            $table->string('logo')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organizations');
     }
 };
---
+++ database/migrations/2023_11_27_004611_create_organization_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organization_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('organization_id');
+            $table->unsignedBigInteger('user_id');
+            // Add other pivot-related fields if needed
+            $table->timestamps();
+
+            // Define foreign key constraints
+            $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organization_user');
+    }
+};
