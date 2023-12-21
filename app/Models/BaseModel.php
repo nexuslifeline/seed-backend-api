bbdc4e6370ef39db7d21ea3033f0321045fad21b
Paul Christian Rueda <nexuslifeline@gmail.com>
2023-12-21 21:27:17 +0800

    Auto fill org id field

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,14 +3,24 @@
 namespace App\Models;
 
 use App\Traits\Auditable;
+use App\Traits\OrgFillable;
 use App\Traits\UuidGenerator;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BaseModel extends Model
 {
-    use Auditable, UuidGenerator;
+    use Auditable, UuidGenerator, OrgFillable, SoftDeletes;
 
     protected $guarded = ['id', 'uuid'];
+
+    /**
+     * Retrieves the name of the route key used for model binding.
+     *
+     * @return string The name of the route key.
+     */
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 }
